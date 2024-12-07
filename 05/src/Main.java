2745e50b187dd2e40a4559b4e03259ca503cd652
Thorsten Hirsch <t.hirsch@web.de>
2024-12-07 22:56:37 +0100

    day 05 - part 2 (and better part 1)

--- 05/src/Main.java
+++ 05/src/Main.java
@@ -2,69 +2,48 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        // read from input.txt
-        var rules = new HashMap<Integer, List<Integer>>();
-        var pages = new ArrayList<List<Integer>>();
+        var rules = new ArrayList<String>();
+        var pages = new ArrayList<List<String>>();
 
+        // comparator of strings is sufficient for task 1 and 2
+        Comparator<String> comparator = (a, b) -> {
+            if (rules.contains(a + "|" + b))
+                return -1;
+            if (rules.contains(b + "|" + a))
+                return 1;
+            return 0;
+        };
+
+        // read from input.txt
         Files.lines(Paths.get("input.txt"))
                 // if line contains "|", then it is a rule, if line contains "," then it is a page
                 .forEach(line -> {
                     if (line.contains("|")) {
-                        var parts = line.split("\\|");
-                        var key = Integer.parseInt(parts[0]);
-                        var value = Integer.parseInt(parts[1]);
-                        if (rules.containsKey(key)) {
-                            rules.get(key).add(value);
-                        } else {
-                            var initialList = new ArrayList<Integer>();
-                            initialList.add(value);
-                            rules.put(key, initialList);
-                        }
+                        rules.add(line);
                     } else if (line.contains(",")) {
-                        var pageList = Arrays.stream(line.split(","))
-                                .mapToInt(Integer::parseInt)
-                                .boxed()
-                                .toList();
-                        //System.out.println("Page list: " + pageList);
-                        pages.add(pageList);
-                    } else if (line.isEmpty()) {
-                        // ignore empty lines
-                    } else {
-                        throw new IllegalArgumentException("Invalid input: " + line);
-                    }
+                        pages.add(Arrays.stream(line.split(",")).toList());
+                    } // ignore other lines
                 });
 
-        // so far, so good, but the next part is ugly
-        var sumOfMiddle = 0;
-        for (var pageList : pages) {
-            try {
-                System.out.print("Checking pages... ");
-                pageList.forEach(page -> {
-                    System.out.print(page + " ");
-                    if (rules.containsKey(page)) {
-                        var mustComeAfter = rules.get(page);
-                        var beforePage = true;
-                        for (var p : pageList) {
-                            if (p.equals(page)) {
-                                beforePage = false;
-                                continue;
-                            }
-                            if (beforePage && mustComeAfter.contains(p))
-                                throw new IllegalStateException("-> Invalid order: " + page + " must come before " + p);
-                        }
-                    }
-                });
-            } catch (IllegalStateException e) {
-                System.out.println(e.getMessage());
-                continue;
+        // task 1 & 2
+        AtomicInteger sumCorrect = new AtomicInteger();
+        AtomicInteger sumWeSorted = new AtomicInteger();
+        pages.forEach(original -> {
+            var sorted = new ArrayList<>(original);
+            sorted.sort(comparator);
+
+            if (original.equals(sorted)) {
+                sumCorrect.addAndGet(Integer.parseInt(original.get((original.size() - 1) / 2)));
+            } else {
+                sumWeSorted.addAndGet(Integer.parseInt(sorted.get((sorted.size() - 1) / 2)));
             }
-            System.out.println("-> OK");
-            sumOfMiddle += pageList.get((pageList.size() - 1) / 2);
-        }
-        System.out.println("Sum of middle elements: " + sumOfMiddle);
+        });
+        System.out.println("Sum of pages already sorted: " + sumCorrect);
+        System.out.println("Sum of pages we had to sort: " + sumWeSorted);
     }
 }
